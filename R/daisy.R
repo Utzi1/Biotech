727da765c428cae061d3d0f45aacfdca82a4a0f7
Utzi1 <lovis.rieger@me.com>
2020-05-19 09:22:28 +0200

    mit HSA1.rd

---
+++ R/daisy.R
@@ -0,0 +1,16 @@
+#' Different values measured during a fermentation of Escherichia coli in a bioreactor named daisy
+#'
+#' @format A tibble: 328 x 10:
+#' \describe{
+#'   \item{Time}{Time}
+#'   \item{STIRR [rpm]}{Rotations per minute of the dispersion element}
+#'   \item{pO2 [%sat]}{Oxygen saturation in %} 
+#'   \item{TEMP [°C]}{Temperature in °C}
+#'   \item{AIR [ccm]}{Airflow in cmm}
+#'   \item{O2 [ccm]}{Oxygen in ccm}
+#'   \item{AFOAMT [ml]}{Antifoam in ml}
+#'   \item{ACIDT [ml]}{Acid in ml (HCl)}
+#'   \item{BASET [ml]}{Base in ml (NaOH)}
+#'   \item{SUBT-A [ml]}{Substrate in ml}
+#' }
+"daisy"
